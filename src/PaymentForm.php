f79fc0a3b1793b014cf7fb8d0c12438a2c39f89b
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2020-04-24 21:03:22 +0300

    Add payment html form renderer

---
+++ src/PaymentForm.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Voronkovich\RaiffeisenBankAcquiring;
+
+class PaymentForm
+{
+    const PROD_URL = 'https://e-commerce.raiffeisen.ru/vsmc3ds/pay_check/3dsproxy_init.jsp';
+    const TEST_URL = 'https://test.ecom.raiffeisen.ru/vsmc3ds/pay_check/3dsproxy_init.jsp';
+
+    private $data;
+    private $label;
+    private $attributes;
+
+    public function __construct(array $data, string $label = 'Pay', array $attributes = [])
+    {
+        $this->data = $data;
+        $this->label = $label;
+        $this->attributes = \array_replace([
+            'action' => self::PROD_URL,
+            'method' => 'POST',
+        ], $attributes);
+    }
+
+    public function render(): string
+    {
+        return \sprintf('<form%s>%s</form>', $this->renderAttributes(), $this->renderBody());
+    }
+
+    public function __toString(): string
+    {
+        return $this->render();
+    }
+
+    private function renderAttributes(): string
+    {
+        $attributes = '';
+        foreach ($this->attributes as $name => $value) {
+            $attributes .= \sprintf(' %s="%s"', $name, $value);
+        }
+
+        return $attributes;
+    }
+
+    private function renderBody(): string
+    {
+        return $this->renderFields().$this->renderButton();
+    }
+
+    private function renderFields(): string
+    {
+        $fields = '';
+        foreach ($this->data as $name => $value) {
+            $fields .= $this->renderField($name, $value);
+        }
+
+        return $fields;
+    }
+
+    private function renderField($name, $value): string
+    {
+        return \sprintf('<input type="hidden" name="%s" value="%s" />', $name, $value);
+    }
+
+    private function renderButton(): string
+    {
+        return \sprintf('<input type="submit" value="%s" />', $this->label);
+    }
+}
---
+++ tests/PaymentFormTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Voronkovich\RaiffeisenBankAcquiring\Tests;
+
+use Voronkovich\RaiffeisenBankAcquiring\PaymentForm;
+use PHPUnit\Framework\TestCase;
+
+class PaymentFormTest extends TestCase
+{
+    public function testRendersHtmlFormForPaymentData()
+    {
+        $data = [
+            'PurchaseAmt' => '30.00',
+            'PurchaseDesc' => '11',
+        ];
+
+        $form = new PaymentForm($data, 'Pay');
+
+        $this->assertEquals(
+            '<form action="https://e-commerce.raiffeisen.ru/vsmc3ds/pay_check/3dsproxy_init.jsp" method="POST"><input type="hidden" name="PurchaseAmt" value="30.00" /><input type="hidden" name="PurchaseDesc" value="11" /><input type="submit" value="Pay" /></form>',
+            (string) $form
+        );
+    }
+}
